f6d3199b226b81f624eee92597da159d9027a003
abc15018045126 <abc15018045126@gmail.com>
2025-01-14 09:46:32 -0500

    1

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    url.hostname = 'sub.cmliussss.workers.dev';
+    url.hostname = 'xuwenjie.abc15018045126.workers.dev';
     return fetch(new Request(url, request));
   },
 };
